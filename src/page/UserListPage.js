f077a154913b2314c4c171f15f6349fb6c11ac43
claire.chen <claire.chen@ubiik.com>
2025-03-26 16:17:23 +0800

    feature: optimize UserListPage

--- src/page/UserListPage.js
+++ src/page/UserListPage.js
@@ -1,45 +1,50 @@
-import React, { useState, useEffect } from "react";
-import UserCard from "../component/UserCard";
-import "../css/page/UserListPage.css";
+import React, { useState, useEffect } from "react"
+import UserCard from "../component/UserCard"
+import AxiosInstance from "../utils/api"
+import "../css/page/UserListPage.css"
 
 const UserListPage = () => {
-    const [userList, setUserList] = useState([]);
+  const [userList, setUserList] = useState([]),
+    [error, setError] = useState(null)
 
-    useEffect(() => {
-        // componentDidMount
-        fetch("https://api.github.com/users?per_page=100&since=0")
-            .then((result) => {
-                return result.json();
-            })
-            .then((userData) => {
-                console.log(userData);
-                setUserList(userData);
-            });
-    }, []);
+  useEffect(() => {
+    AxiosInstance.get(`/users?per_page=100&since=0`)
+      .then((res) => {
+        setUserList(res.data)
+      })
+      .catch((error) => setError(error))
+  }, [])
 
-    return (
-        <>
-            <div className="list-container">
-                <h1 className="list-title ">Github User</h1>
-                <ul className="card-group-container d-flex flex-wrap">
-                    {userList.length ? (
-                        userList.map((el) => {
-                            return (
-                                <UserCard
-                                    key={el.id}
-                                    login={el.login}
-                                    avatar_url={el.avatar_url}
-                                    site_admin={el.site_admin}
-                                />
-                            );
-                        })
-                    ) : (
-                        <p>none</p>
-                    )}
-                </ul>
-            </div>
+  return (
+    <div className="list-container">
+      {error
+        ? <>
+          <div className="d-flex justify-content-center">
+            <h1 className="error">{error}</h1>
+          </div>
         </>
-    );
-};
+        : (
+          <>
+            <h1 className="list-title">Github Users</h1>
+            <ul className="card-group-container d-flex flex-wrap">
+              {userList.length ? (
+                userList?.map((el) => (
+                  <UserCard
+                    key={el.id}
+                    login={el.login}
+                    avatar_url={el.avatar_url}
+                    site_admin={el.site_admin}
+                  />
+                ))
+              ) : (
+                <p>none</p>
+              )}
+            </ul>
+            )
+          </>
+        )}
+    </div>
+  )
+}
 
-export default UserListPage;
+export default UserListPage
